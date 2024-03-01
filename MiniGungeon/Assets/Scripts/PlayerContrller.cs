4295e026586cc88be69e9fc1f6a35408b0751639
hoyoung1359 <hoyoung1359@yonsei.ac.kr>
2024-03-01 22:23:06 +0900

    50분까지
    그래픽 에셋 다운 및 임포트, 타일맵 자르기, 플로에어 캐릭터와 벽 충돌처리, 입력시 움직임.

---
+++ MiniGungeon/Assets/Scripts/PlayerContrller.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContrller : MonoBehaviour
+{
+
+    public float speed = 3;
+    Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            move += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            move += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            move += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            move += new Vector3(0, -1, 0);
+        }
+
+        move = move.normalized;
+
+        if (move.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (move.x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+
+
+
+
+}
